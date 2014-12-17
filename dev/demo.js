e6a3592b67a41193a207cbd3ed1782dea22fa1a2
VincentLoy <vincent.loy1@gmail.com>
2014-12-17 03:53:17 +0100

    jquery.tweetParser added + demo

---
+++ dev/demo.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ 
+    
+    $(".tweet").tweetParser();
+    
+    //you can also custom css classes and target like this
+    /*
+    $(".tweet").tweetParser({
+        urlClass : "tweet_link", //this is default
+        userClass : "tweet_user", //this is default
+        hashtagClass : "hashtag", //this is default
+        target : "_blank" //this is default
+    });
+    */
+    
+});
