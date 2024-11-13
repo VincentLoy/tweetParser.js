7d34e4037a02c8af07035fa0e9c65dfeec9a7fba
Vincent Loy <vincent.loy1@gmail.com>
2024-11-13 14:15:43 +0100

    Update averything 2024 way. Also add feature suggested in #14 - fix #14

--- dev/tweetParser.js
+++ dev/tweetParser.js
@@ -1,117 +1,108 @@
 /*!
- * tweetParser.js v2.2.0
- * Small Javascript Library that parse an element containing a tweet and turn URLS, @user & #hashtags into working urls
- * License : MIT
- * author Vincent Loy <vincent.loy1@gmail.com>
+ * tweetParser.js v3.0.0
+ * A JavaScript library that parses elements containing tweets, transforming URLs, @usernames, and #hashtags into clickable links for both x.com and twitter.com
+ * License: MIT
+ * Author: Vincent Loy <vincent.loy1@gmail.com>
  * http://vincent-loy.fr
  */
-
-/*global window, document*/
-/*jslint regexp: true*/
 
 (function (exports) {
     'use strict';
 
-    // Class
-    let tweetParser,
+    // Constants
+    const REGEX_URL = /(?:\s)(f|ht)tps?:\/\/([^\s\t\r\n<]*[^\s\t\r\n<)*_,\.])/g; // regex for URLs
+    const REGEX_USER = /\B@([a-zA-Z0-9_]+)/g; // regex for @usernames
+    const REGEX_HASHTAG = /\B(#[á-úÁ-Úä-üÄ-Üa-zA-Z0-9_]+)/g; // regex for #hashtags
 
-    // functions
-        extend,
-        generateLink;
+    // Default parameters
+    const DEFAULT_OPTIONS = {
+        urlClass: 'tweet_link',
+        userClass: 'tweet_user',
+        hashtagClass: 'hashtag',
+        target: '_blank',
+        searchWithHashtags: true,
+        parseUsers: true,
+        parseHashtags: true,
+        parseUrls: true,
+        baseURL: 'https://x.com', // Default domain for links
+        hideUserSymbol: false, // New: Hide "@" in @username
+        hideProtocol: false // New: Hide "https://" or "http://" in displayed text for URLs
+    };
 
-    extend = function (out) {
-        out = out || {};
-
-        for (let i = 1; i < arguments.length; i += 1) {
-            if (arguments[i]) {
-                for (let key in arguments[i]) {
-                    if (arguments[i].hasOwnProperty(key)) {
-                        out[key] = arguments[i][key];
-                    }
-                }
+    // Helper function to extend objects (shallow copy)
+    const extend = (out = {}, ...args) => {
+        args.forEach(arg => {
+            if (arg) {
+                Object.keys(arg).forEach(key => {
+                    out[key] = arg[key];
+                });
             }
-        }
+        });
         return out;
     };
 
-    generateLink = function (url, className, target, text) {
-        let link = document.createElement('a');
-        link.href = url;
+    // Helper function to generate a link element
+    const generateLink = (url, className, target, text) => {
+        const link = document.createElement('a');
+        link.href = url; // Keep the full URL with protocol in the href
         link.classList.add(className);
         link.target = target;
-        link.textContent = text;
-
+        link.textContent = text; // Display text (can have protocol hidden if specified)
         return link;
     };
 
-    tweetParser = function (element, args) {
-        let elt = document.querySelectorAll(element),
-            parameters = extend({
-                urlClass: 'tweet_link',
-                userClass: 'tweet_user',
-                hashtagClass: 'hashtag',
-                target: '_blank',
-                searchWithHashtags: true,
-                parseUsers: true,
-                parseHashtags: true,
-                parseUrls: true
-            }, args);
+    // Main tweetParser function
+    const tweetParser = (element, options = {}) => {
+        const elements = document.querySelectorAll(element);
+        const parameters = extend({}, DEFAULT_OPTIONS, options);
 
-        Array.prototype.forEach.call(elt, el => {
+        // Define hashtag search URL based on baseURL
+        const hashtagBaseURL = parameters.searchWithHashtags 
+            ? `${parameters.baseURL}/hashtag/`
+            : `${parameters.baseURL}/search?q=`;
 
-            const REGEX_URL = /(?:\s)(f|ht)tps?:\/\/([^\s\t\r\n<]*[^\s\t\r\n<)*_,\.])/g, //regex for urls
-                REGEX_USER = /\B@([a-zA-Z0-9_]+)/g, //regex for @users
-                REGEX_HASHTAG = /\B(#[á-úÁ-Úä-üÄ-Üa-zA-Z0-9_]+)/g; //regex for #hashtags
+        elements.forEach(el => {
+            let content = el.innerHTML;
 
-            let tweet = el.innerHTML,
-                searchlink; //search link for hashtags
-
-            //Hashtag Search link
-            if (parameters.searchWithHashtags) {
-                //this is the search with hashtag
-                searchlink = "https://twitter.com/hashtag/";
-            } else {
-                //this is a more global search including hashtags and the word itself
-                searchlink = "https://twitter.com/search?q=";
-            }
-
-            //turn URLS in the tweet into... working urls
+            // Replace URLs with links
             if (parameters.parseUrls) {
-                tweet = tweet.replace(REGEX_URL, function (url) {
-                    let link = generateLink(url, parameters.urlClass, parameters.target, url);
-
+                content = content.replace(REGEX_URL, url => {
+                    // If hideProtocol is true, remove protocol from displayed text
+                    const displayUrl = parameters.hideProtocol ? url.replace(/https?:\/\//, '') : url;
+                    const link = generateLink(url, parameters.urlClass, parameters.target, displayUrl);
                     return url.replace(url, link.outerHTML);
                 });
             }
 
-            //turn @users in the tweet into... working urls
+            // Replace @usernames with links, optionally hiding the @ symbol
             if (parameters.parseUsers) {
-                tweet = tweet.replace(REGEX_USER, function (user) {
-                    let userOnly = user.slice(1),
-                        url = `http://twitter.com/${userOnly}`,
-                        link = generateLink(url, parameters.userClass, parameters.target, user);
-
+                content = content.replace(REGEX_USER, user => {
+                    const userOnly = user.slice(1);
+                    const url = `${parameters.baseURL}/${userOnly}`;
+                    // If hideUserSymbol is true, display username without the @ symbol
+                    const displayUser = parameters.hideUserSymbol ? userOnly : user;
+                    const link = generateLink(url, parameters.userClass, parameters.target, displayUser);
                     return user.replace(user, link.outerHTML);
                 });
             }
 
-            //turn #hashtags in the tweet into... working urls
+            // Replace #hashtags with links
             if (parameters.parseHashtags) {
-                tweet = tweet.replace(REGEX_HASHTAG, function (hashtag) {
-                    let hashtagOnly = hashtag.slice(1),
-                        url = searchlink + hashtagOnly,
-                        link = generateLink(url, parameters.hashtagClass, parameters.target, hashtag);
-
+                content = content.replace(REGEX_HASHTAG, hashtag => {
+                    const hashtagOnly = hashtag.slice(1);
+                    const url = `${hashtagBaseURL}${encodeURIComponent(hashtagOnly)}`;
+                    const link = generateLink(url, parameters.hashtagClass, parameters.target, hashtag);
                     return hashtag.replace(hashtag, link.outerHTML);
                 });
             }
 
-            //then, it inject the last var into the element containing the tweet
-            el.innerHTML = tweet;
+            // Update the element's HTML with parsed content
+            el.innerHTML = content;
         });
     };
 
     exports.tweetParser = tweetParser;
+
 }(window));
 
 /*global $, jQuery, tweetParser*/
