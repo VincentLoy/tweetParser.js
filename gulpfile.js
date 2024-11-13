5760bb998c0fe48cb40331a987c54cfb552baea0
Vincent Loy <vincent.loy1@gmail.com>
2024-11-13 13:15:06 +0100

    Remove Bower - Update all dev dependecies - node 22.11 compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /**
  * Project : tweetParser.js
  * File : gulpfile
- * Date : 06/20/2015
+ * Date : 11/13/2024
  * Author : Vincent Loy <vincent.loy1@gmail.com>
  */
 
@@ -10,59 +10,66 @@
 (function () {
     'use strict';
 
-    var gulp = require('gulp'),
-        gutil = require('gulp-util'),
+    const gulp = require('gulp'),
+        log = require('fancy-log'),
         less = require('gulp-less'),
-        minifyCSS = require('gulp-minify-css'),
+        cleanCSS = require('gulp-clean-css'),
         sourcemaps = require('gulp-sourcemaps'),
         plumber = require('gulp-plumber'),
-        uglify = require('gulp-uglify'),
+        terser = require('gulp-terser'), // replacing gulp-uglify with gulp-terser
         rename = require('gulp-rename'),
         babel = require('gulp-babel'),
         targetCSSDir = 'demo',
-        targetLESSDir = targetCSSDir + '/less',
+        targetLESSDir = `${targetCSSDir}/less`,
         devJSDir = 'dev',
         distJSDir = 'dist';
 
-
-    // Compile Less
-    // and save to target CSS directory
-    gulp.task('css', function () {
-        return gulp.src(targetLESSDir + '/demo.less')
+    // Compile Less and minify CSS
+    function css() {
+        return gulp.src(`${targetLESSDir}/demo.less`)
             .pipe(plumber())
-            .pipe(less({style: 'compressed'})
-                .on('error', gutil.log))
+            .pipe(less({ javascriptEnabled: true }))
+            .on('error', log)
             .pipe(sourcemaps.init())
-            .pipe(minifyCSS({compatibility: 'ie8'}))
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
             .pipe(sourcemaps.write())
             .pipe(gulp.dest(targetCSSDir));
-    });
+    }
 
-    gulp.task('build:es6', function () {
-        return gulp.src(devJSDir + '/tweetParser.js')
+    // Transpile ES6, minify, and generate sourcemaps for JavaScript
+    function buildES6() {
+        return gulp.src(`${devJSDir}/tweetParser.js`)
             .pipe(sourcemaps.init())
             .pipe(babel({
-                presets: ['babel-preset-es2015']
+                presets: ['@babel/preset-env']
             }))
-            .pipe(uglify({
-                preserveComments: 'some'
+            .pipe(terser({
+                format: {
+                    comments: 'some' // preserve some comments
+                }
             }))
             .pipe(rename({
                 extname: '.min.js'
             }))
             .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest(distJSDir));
-    });
+    }
 
-    // Keep an eye on Less
-    gulp.task('watch', function () {
-        gulp.watch('demo/**/*.less', ['css']);
-        gulp.watch('./dev/tweetParser.js', ['build:es6']);
-    });
+    // Watch for changes in LESS and JavaScript files
+    function watchFiles() {
+        gulp.watch('demo/**/*.less', css);
+        gulp.watch(`${devJSDir}/tweetParser.js`, buildES6);
+    }
 
-    // What tasks does running gulp trigger?
-    gulp.task('default', ['css', 'watch']);
+    // Define complex tasks
+    const build = gulp.series(css, buildES6);
+    const watch = gulp.parallel(watchFiles);
 
-    //To uglify the new version of tweetparser run : gulp release or gulp build:es6
-    gulp.task('release', ['build:es6']);
+    // Export tasks
+    exports.css = css;
+    exports.buildES6 = buildES6;
+    exports.watch = watch;
+    exports.default = gulp.series(build, watch);
+    exports.release = build; // To run with 'gulp release'
+
 }());
